dbf526673e27c05fd27dec5acff07e824698fea5
Seedak Preet Singh Taneja <sd524713@dal.ca>
2021-07-25 09:40:00 -0300

    Drop query to handle multiple tables.

--- src/main/java/org/group15/sql/Drop.java
+++ src/main/java/org/group15/sql/Drop.java
@@ -18,24 +18,32 @@ public class Drop {
   public boolean parseDropTableStatement(String query, String schemaName) throws Exception {
     String[] queryParts = query.split("\\s+");
 
-    if (queryParts.length != 3) {
+    if (queryParts.length < 3) {
       this.eventLogsWriter.append("Syntax error: Error while parsing drop " +
-          "table query").append("\n");
+              "table query").append("\n");
       this.eventLogsWriter.close();
       throw new Exception("Syntax error: Error while parsing drop table query");
     }
 
     if (!queryParts[1].equalsIgnoreCase("TABLE")) {
       this.eventLogsWriter.append("Syntax error: TABLE keyword not found " +
-          "in drop table query").append("\n");
+              "in drop table query").append("\n");
       this.eventLogsWriter.close();
       throw new Exception("Syntax error: TABLE keyword not found in " +
-          "drop table query");
+              "drop table query");
     }
+    if(queryParts.length == 3){
+      String tableName = queryParts[2];
 
-    String tableName = queryParts[2];
-
-    return table.dropTable(schemaName, tableName);
+      return table.dropTable(schemaName, tableName);
+    }
+    else{
+      for(int i =2; i < queryParts.length; i++){
+        String tableName = queryParts[i];
+        tableName = tableName.replaceAll(",$", "");
+        table.dropTable(schemaName, tableName);
+      }
+    }
+    return true;
   }
-
 }
