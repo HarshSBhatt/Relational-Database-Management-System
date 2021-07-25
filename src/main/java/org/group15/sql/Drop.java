129fe6f0b504ce1a2747a3ef52f50291d45f6f2f
Harsh Bhatt <harshb669@gmail.com>
2021-07-25 14:16:05 +0530

    drop table logic done

--- src/main/java/org/group15/sql/Drop.java
+++ src/main/java/org/group15/sql/Drop.java
@@ -1,13 +1,41 @@
 package org.group15.sql;
 
+import org.group15.database.Table;
+
 import java.io.FileWriter;
 
 public class Drop {
 
   FileWriter eventLogsWriter;
 
+  Table table;
+
   public Drop(FileWriter eventLogsWriter) {
     this.eventLogsWriter = eventLogsWriter;
+    table = new Table(eventLogsWriter);
+  }
+
+  public boolean parseDropTableStatement(String query, String schemaName) throws Exception {
+    String[] queryParts = query.split("\\s+");
+
+    if (queryParts.length != 3) {
+      this.eventLogsWriter.append("Syntax error: Error while parsing drop " +
+          "table query").append("\n");
+      this.eventLogsWriter.close();
+      throw new Exception("Syntax error: Error while parsing drop table query");
+    }
+
+    if (!queryParts[1].equalsIgnoreCase("TABLE")) {
+      this.eventLogsWriter.append("Syntax error: TABLE keyword not found " +
+          "in drop table query").append("\n");
+      this.eventLogsWriter.close();
+      throw new Exception("Syntax error: TABLE keyword not found in " +
+          "drop table query");
+    }
+
+    String tableName = queryParts[2];
+
+    return table.dropTable(schemaName, tableName);
   }
 
 }
