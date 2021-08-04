46ff5eaa95f6a4404229948901a364c7a41fee39
Harsh Bhatt <harshb669@gmail.com>
2021-08-04 11:02:05 +0530

    misc

--- src/test/java/org/group15/AppTest.java
+++ src/test/java/org/group15/AppTest.java
@@ -1,20 +1,20 @@
 package org.group15;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+
 }
